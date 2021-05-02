4917a3823cec0615ed6f9ffcf90d8a1e6c2124f9
Theresa Pidutti <treepidutti@gmail.com>
2021-05-02 17:04:04 -0600

    update components

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Jumbotron, Container, Card, Form, Button } from "react-bootstrap";
+
+function Jumbo() {
+return (
+<Jumbotron fluid>
+  <Container>
+    <Card style={{ width: "18rem" }}>
+      <Card.Body>
+        <Card.Title>Welcome to Paw Prints</Card.Title>
+        <Card.Text>
+          Find your new best friend.
+          lkadjl;fkjkasdlkjglsdkglsdj;jfsldkfjlskdlgsdllfkjsljdsl
+        </Card.Text>
+        <Form>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Label>City</Form.Label>
+              <Form.Control />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridState">
+              <Form.Label>State</Form.Label>
+              <Form.Control as="select" defaultValue="Choose...">
+                <option>Choose...</option>
+                <option>...</option>
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridZip">
+              <Form.Label>Zip</Form.Label>
+              <Form.Control />
+            </Form.Group>
+          </Form.Row>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+    </Card>
+  </Container>
+</Jumbotron>
+);
+}
+
+export default Jumbo;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pawprints");
 
 // Start the API server
 app.listen(PORT, function() {
