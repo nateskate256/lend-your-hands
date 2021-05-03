303481f7adef14d78ed928d6c137c5f273771085
Nate Shipp <nateskate256@gmail.com>
2021-05-03 10:11:07 -0700

    trying to render

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Modal, Button, Form } from "react-bootstrap";
 
 function ConfirmationBox() {
--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  // app.get('*', (req, res) => {
+  //   res.sendFile(__dirname + '/client/build/index.html');
+  // });
+}
+// Add routes, both API and view
+app.use(routes);
+
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pawprints");
 
@@ -12,3 +25,4 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pawprints");
 app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
   });
+
