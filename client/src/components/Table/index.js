aea2180f847967f44f74619512607fca6e843135
Theresa Pidutti <treepidutti@gmail.com>
2021-05-05 10:23:18 -0600

    hardcode table for ref

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -6,7 +6,7 @@ function Table() {
   const token = getAuth();
   console.log(token);
   return (
-    <table striped bordered>
+    <table className="table striped-table">
       <thead>
         <tr>
           <th scope="col">Name</th>
@@ -19,6 +19,18 @@ function Table() {
           <th scope="col">Date</th>
         </tr>
       </thead>
+
+      <tbody>
+        <td>Spot</td>
+        <td>Cat</td>
+        <td>American shorhair</td>
+        <td>Male</td>
+        <td>2 years</td>
+        <td>photo</td>
+        <td>890-234-4567</td>
+        <td>May 5, 2021</td>
+      </tbody>
+
       {/* <tbody>
         {props.users.map((data) => {
           return (
