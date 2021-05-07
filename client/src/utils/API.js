3277730b6cf922e4b866a4adf66c45f3d31b143a
Nate Shipp <nateskate256@gmail.com>
2021-05-07 10:12:07 -0700

    setting up token

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,8 +2,50 @@ import axios from "axios";
 
 export default {
   getLocalPets: function () {
-    return axios.get("https://api.petfinder.com/v2/animals?location=arizona&limit=10", {headers:{"Authorization":"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJSdVBPbVpIY3FLWEVGclRmZGwyN25vYjR6VVVFaDhra3BuUFdsRkJRUWRlODlFVmNhMiIsImp0aSI6ImJkNDQ0ZGJlZTJmZDJjOWNmOTRiYWU4ZDRmOGRiNDI1YjE0ZmMyNzBjN2FkNDAxYjg3NzIyOWMxM2M1MzNkMzMwMjBjNThmNDg4Y2UyODdmIiwiaWF0IjoxNjIwMjQ4MDcwLCJuYmYiOjE2MjAyNDgwNzAsImV4cCI6MTYyMDI1MTY3MCwic3ViIjoiIiwic2NvcGVzIjpbXX0.Z9-P8HOJAlDXNatGqBz29NqJFJihRbDQe7xuyYlCF0ySJVMWMhnZxRIzvpg3Sp4cTgkIvp1IxZnvjvzHQ1zi1T-qemvN_qt4nMVbLbI_5FWQl-cqlfnXjZ2dkCGhXz0p_CeVXzHWpkt8AbeopJwZylXEtJPgUsE8WQd_PuumNQvjphMEfzED3mEpVOdmmvvwXKFtn8W_75tfBrQXm3Mp1CXA1SD58YwDF9CwIYtdjRSJ6EUZjS6EFj3nwgWTbp4BR6pTCrM6Ri7idqfR5BCszI11IpQycs_vgItLxrLuqo1PfHf6kevV9TrtbFMgFWbUVMFAvQ-9noFIFC3iG7qYKw"}})
+    return axios.get("/api/pets")
+  },
+
+  getOAuthToken: function (){
+    return axios.get("/api/petstoken")
   }
+
+  // // getOAuthToken: async function () {
+  //   try {
+  //     // const resp = await axios.post('https://api.petfinder.com/v2/oauth2/token',
+  //     //     {params:{
+  //     //       grant_type: "client_credentials",
+  //     //       // client_id: process.env.REACT_APP_PETFINDER_API,
+  //     //       // client_secret: process.env.REACT_APP_PETFINDER_SECRET,
+  //     //       client_id: "RuPOmZHcqKXEFrTfdl27nob4zUUEh8kkpnPWlFBQQde89EVca2",
+  //     //       client_secret: "rzk6a0134syYve69kDPw3zDHJO44db7Bw4BAM5SU",
+  //     //     }},
+  //     //     {headers:{
+  //     //       "Content-Type":'x-www-form-urlencoded'
+  //     //     }}
+  //     //   );
+  //     const resp = await axios({
+  //       url: "https://api.petfinder.com/v2/oauth/token",
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "x-www-form-urlencoded"
+  //       },
+  //       body: {
+  //         client_id: "RuPOmZHcqKXEFrTfdl27nob4zUUEh8kkpnPWlFBQQde89EVca2",
+  //         client_secret: "rzk6a0134syYve69kDPw3zDHJO44db7Bw4BAM5SU",
+  //         grant_type:"client-credentials"
+  //       },
+  //       });
+  //        let token = resp.access_token;
+  //     let tokenType = resp.token_type;
+  //     let expires = new Date().getTime() + (resp.expires_in * 1000)
+
+  //     console.log(resp)
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
+
+}
 
 
 
@@ -22,4 +64,3 @@ export default {
   // saveAnimals: function (animalsData) {
   //   return axios.post("/api/animals", animalsData);
   // },
-};
---
+++ routes/api/pets.js
@@ -0,0 +1,96 @@
+const router = require("express").Router();
+const axios = require('axios');
+
+router.get("/", async (req, res) => {
+    try {
+        const response = await axios.get("https://api.petfinder.com/v2/animals?location=arizona&limit=15", { headers: { "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJSdVBPbVpIY3FLWEVGclRmZGwyN25vYjR6VVVFaDhra3BuUFdsRkJRUWRlODlFVmNhMiIsImp0aSI6IjUwMzMyZTU3ODc2ZWFiMDgwYWY0YjdmOWQyYjZhNjE4YTAwZTA0ZmZiNWFhMWFhZmUyN2RiZTc3OTAyOTc1NGMwZDI5NzBiYTY4MDUyNTRlIiwiaWF0IjoxNjIwMzQwNDQ0LCJuYmYiOjE2MjAzNDA0NDQsImV4cCI6MTYyMDM0NDA0NCwic3ViIjoiIiwic2NvcGVzIjpbXX0.ITmje9Db0LfeqFw8mrIPYBs1Ztjdv2XKXb7EqpXFOFH0O80Tkm1lwxJ567NcjBXNHcuYEpPKCXWze-ADsycmj9Hez6VYsRkFRSPSitR9cFeE8F5VReTIK6GANZmhx1mYMfheTLe0_uFTakPP7HamSsfr11h75ygPxzVsa1DMENxsCqEzZo42tDQDtW4XNyoDIO0KFrFfjiQ-wNbMMyZR9Tpl8c5XHxsYPNPdMBWmueBYzo09SCE8LHB-CWAVbtqtdbmzDJt2Fyf1Xf6KFs267vMvE4Loc_ylsVNrAyVbiJ1rY1LWkwmryTkCg9ZeMGnri2w-6WGiU9iLLB_ipTuEOw" } })
+        res.json(response.data)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+router.get('/token', async (req, res) => {
+    try {
+        const response = await axios({
+
+            url: "https://api.petfinder.com/v2/oauth/token",
+            method: "POST",
+            headers: {
+                "Content-Type": "x-www-form-urlencoded"
+            },
+            data: {
+                client_id: "RuPOmZHcqKXEFrTfdl27nob4zUUEh8kkpnPWlFBQQde89EVca2",
+                client_secret: "rzk6a0134syYve69kDPw3zDHJO44db7Bw4BAM5SU",
+                grant_type: "client_credentials"
+            },
+        });
+        // let token = response.access_token;
+        // let tokenType = response.token_type;
+        // let expires = new Date().getTime() + (response.expires_in * 1000)
+        res.json(response)
+    } catch (err) {
+        console.log(err)
+    }
+})
+      
+
+
+module.exports = router;
+
+// const { default: Axios } = require("axios");
+// import { useState } from "react";
+
+// const router = require("express").Router();
+
+// state = { location };
+// const [petData, setpetData] = useState("");
+// let token,
+//   tokenType,
+//   expires = "";
+// const location = this.state.location;
+
+// const getOAuth = async () => {
+//   try {
+//     const getToken = await Axios.post(
+//       "https://api.petfinder.com/v2/oauth2/token",
+//       {
+//         method: "POST",
+//         body: JSON.stringify({
+//           grant_type: "client_credentials",
+//           client_id: process.env.REACT_APP_PETFINDER_API,
+//           client_secret: process.env.REACT_APP_PETFINDER_SECRET,
+//         }),
+//         headers: {
+//             "Content-Type": "application/x-www-form-urlencoded",
+//           },
+//       }
+//     );
+//     getToken = getToken.json();
+//     // Store token data
+//     token = getToken.access_token;
+//     tokenType = getToken.token_type;
+//     expires = new Date().getTime() + getToken.expires_in * 1000;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// const getPetData = async () => {
+//   const response = await Axios.get(
+//     `https://api.petfinder.com/v2/animals?location=${location}&limit=50`,
+//     { headers: { Authorization: `Bearer ${token}` } }
+//   );
+//   console.log(response);
+//   setpetData(response.data);
+// };
+
+// // Make call if token expired
+// const makeCall = async () => {
+//   // If current token is invalid, get a new
+//   await getOAuth();
+//   getPetData();
+// };
+
+// makeCall();
+
